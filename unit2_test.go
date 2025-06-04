19ca247e7dad0b2448220301948d0e7f2adf3999
ninza <ninzasingh44@gmail.com>
2025-06-04 14:59:26 +0530

    Update unit2_test.go

--- unit2_test.go
+++ unit2_test.go
@@ -30,3 +30,42 @@ func TestValidateMinLength(t *testing.T){
 		}
 
 }
+
+
+func TestValidateMaxLength(t *testing.T) {
+	tests := []struct {
+		rule string 
+		field reflect.Value
+		fieldName string
+		wantErr bool
+
+	}{
+		{"max=2", reflect.ValueOf("Alice"), "Name",true},
+		{"max=5", reflect.ValueOf("Alice"), "Name",false},
+		{"max=0", reflect.ValueOf(""), "Name",false},
+	}
+	for _, tt := range tests {
+		err := validateMaxLength(tt.rule, tt.field, tt.fieldName)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("validateMaxLength() error = %v, wantErr %v", err, tt.wantErr)
+		}
+	}
+}
+
+func TestValidateRequired(t *testing.T) {
+	tests := []struct {
+		field reflect.Value
+		fieldName string
+		wantErr bool
+	}
+	{
+		{reflect.ValueOf(""), "Email", true},
+		{reflect.ValueOf("alice@example.com"),"Email",false},
+	}
+	for _, tt  := range tests {
+		err := validateRequired(tt.field, tt.fieldName)
+		if (err != nil) != tt.wantErr {
+			t.Errorf("validateReuired() error = %v,wantErr %v", err, tt.wantErr)
+		}
+	}
+}
